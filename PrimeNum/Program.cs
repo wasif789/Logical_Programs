2124aedbc45ecb8ba67c746615c210887f26d6a0
wasif farid <wsffarid1@gmail.com>
2021-09-03 19:45:59 +0530

    prime number added

---
+++ PrimeNum/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PrimeNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the number:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int a = 0;
+            for (int i=1;i<=num;i++)
+            {
+                if(num%i==0)
+                {
+                    a++;
+                }
+            }
+            if(a==2)
+            {
+                Console.WriteLine(num+" is a prime number");
+            }
+            else
+            {
+                Console.WriteLine(num+" is not a prime number");
+            }
+        }
+    }
+}
