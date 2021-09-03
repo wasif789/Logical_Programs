118a8e8f91551e6630d10fe33b87cca621c54383
wasif farid <wsffarid1@gmail.com>
2021-09-03 19:31:16 +0530

    perfect number

---
+++ perfectNum/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace perfectNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, sum = 0;
+            Console.WriteLine("enter the number:");
+            num = Convert.ToInt32(Console.ReadLine());
+            for(int i=1;i<num;i++)
+            {
+                if(num%i==0)
+                {
+                    sum =sum+i;
+                }
+            }
+            if(sum==num)
+            {
+                Console.WriteLine("number is a perfect number");
+            }
+            else
+            {
+                Console.WriteLine("number is not a perfect number" );
+            }
+        }
+    }
+}
