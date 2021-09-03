ac99ccf05b17b46f1506a9a98d1456c0cb9ff465
wasif farid <wsffarid1@gmail.com>
2021-09-03 19:11:56 +0530

    fabonacci series

---
+++ Fabonacci/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Fabonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 0, n2 = 1, n3, i, num;
+            Console.Write("Enter the number of elements: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            Console.Write(n1 + " " + n2 + " "); //printing 0 and 1    
+            for (i = 2; i < num; ++i) //loop starts from 2 because 0 and 1 are already printed    
+            {
+                n3 = n1 + n2;
+                Console.Write(n3 + " ");
+                n1 = n2;
+                n2 = n3;
+            }
+        }
+    }
+}
