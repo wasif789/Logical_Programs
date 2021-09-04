76c9638a96cef516ec9a0cb4bec850a365198974
wasif farid <wsffarid1@gmail.com>
2021-09-04 18:42:43 +0530

    stop watch

---
+++ StopWatch/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace StopWatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create new stopwatch
+            Stopwatch stopWatch = new Stopwatch();
+            //Begin Timimg
+            stopWatch.Start();
+            //do something
+            for (int i = 0; i < 1000; i++)
+            {
+                Thread.Sleep(10);
+            }
+            //stop timing
+            stopWatch.Stop();
+            //printing result
+            Console.WriteLine("Time Elapsed:{0}", stopWatch.Elapsed);
+
+        }
+    }
+}
