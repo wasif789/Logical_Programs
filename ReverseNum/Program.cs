237d37f8f7c3c00af3fa9c07ca6c9db868b5caca
wasif farid <wsffarid1@gmail.com>
2021-09-04 18:29:16 +0530

    reverse a number

---
+++ ReverseNum/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ReverseNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, reverse = 0, rem;
+            Console.WriteLine("Enter a number: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            while (n != 0)
+            {
+                rem = n % 10;
+                reverse = reverse * 10 + rem;
+                n /= 10;
+            }
+            Console.WriteLine("Reversed Number: " + reverse);
+        }
+    }
+}
