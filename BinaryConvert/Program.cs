e345eb82f069054ceed84c72d02b021385ccae54
wasif farid <wsffarid1@gmail.com>
2021-09-04 21:55:42 +0530

    binary conversion

---
+++ BinaryConvert/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BinaryConvert
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            DecimalToBinary.ReadInput();
+            Console.ReadLine();
+
+        }
+    }
+    class DecimalToBinary
+    {
+        public static void ReadInput()
+        {
+            //Read input from user
+            Console.WriteLine("Enter decimal number");
+            int decimalNumber = Convert.ToInt32(Console.ReadLine());
+            DecimalToBinaryConversion(decimalNumber);
+        }
+
+
+        private static void DecimalToBinaryConversion(int decimalNumber)
+        {
+            //Local variable to iterate
+            int count;
+            //Store value of binary output
+            List<int> binaryNumber = new List<int>();
+            for (count = 0; decimalNumber > 0; count++)
+            {
+                binaryNumber.Add(decimalNumber % 2);
+                decimalNumber = decimalNumber / 2;
+            }
+            //Print BInary Representation
+            Console.Write("Binary Representation is =\t");
+            for (count = binaryNumber.Count - 1; count > 0; count--)
+            {
+                Console.Write(binaryNumber[count]);
+            }
+        }
+    }
+}
