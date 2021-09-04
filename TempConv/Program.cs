719c64e2a7d1104a57a5ae24405fba4d19704b7c
wasif farid <wsffarid1@gmail.com>
2021-09-04 19:04:55 +0530

    temprature convertion

---
+++ TempConv/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TempConv
+{
+    class Program
+    {
+        double celsius;
+        double fahrenheit;
+        static void Main(string[] args)
+        {
+            double celsius;
+            double fahrenheit;
+            Console.WriteLine("Enter value for celsius:");
+            celsius = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter value for Fahrenheit:");
+            fahrenheit = Convert.ToDouble(Console.ReadLine());
+            double celToFah = (celsius * 9 / 5) + 32;
+            double fahToCel = (fahrenheit - 32) * 5 / 9;
+            Console.WriteLine("After celsius to fahrenheit conversion: {0}", celToFah);
+            Console.WriteLine("After fahrenheit to celsius conversion: {0}", fahToCel);
+        }
+    }
+}
