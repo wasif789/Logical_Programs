8cdb126b3ab5903310dc463a49ece0c9daf66b23
wasif farid <wsffarid1@gmail.com>
2021-09-04 21:50:14 +0530

    monthly pyment added

---
+++ MonthlyPayment/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MonthlyPayment
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Input.MonthlyPayment();
+            Console.ReadLine();
+
+        }
+    }
+    class Input
+    {
+        public static void MonthlyPayment()
+        {
+            Console.WriteLine("Enter principle");
+            double p = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter years");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter rate");
+            double R = Convert.ToInt32(Console.ReadLine());
+            int n = 12 * y;
+            double r = (R / (12 * 100));
+            double s = 1 + r;
+            double power = (double)Math.Pow(s, -n);
+            double payment = (p * r) / power;
+            Console.WriteLine("Monthly payment is :{0}", payment);
+        }
+    }
+}
