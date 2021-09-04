af4ebfbda53fc9ebb3ff6eb8ae91c59ee8531b00
wasif farid <wsffarid1@gmail.com>
2021-09-04 21:44:58 +0530

    sqare root added

---
+++ SqRoot/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace SqRoot
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            NewtonSqrt.ReadInput();
+            Console.ReadLine();
+
+        }
+    }
+    class NewtonSqrt
+    {
+        public static void ReadInput()
+        {
+            //Read input from user
+            Console.WriteLine("Enter value of c");
+            double c = Convert.ToDouble(Console.ReadLine());
+            NewtonSquareRoot(c);
+        }
+
+
+        private static void NewtonSquareRoot(double c)
+        {
+            //Given: local variables
+            double t = c;
+
+            //Given l= 1^-5 = 0.00001
+            double l = 0.00001;
+            double sqrtValue;
+
+            while (true)
+            {
+                //Compute average of c/t and t
+                sqrtValue = 0.5 * (t + (c / t));
+                if (Math.Abs(sqrtValue - t) < l)
+                {
+                    break;
+                }
+                t = sqrtValue;
+            }
+            Console.WriteLine("Root Value is :" + Math.Round(sqrtValue, 4));
+
+
+        }
+    }
+}
+
