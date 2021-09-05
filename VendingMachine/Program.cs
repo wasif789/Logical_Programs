a03380666156d2f9970551331d66d3fa87d017e6
wasif farid <wsffarid1@gmail.com>
2021-09-05 23:27:41 +0530

    vending machine

---
+++ VendingMachine/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace VendingMachine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Menu\n1.Vending Machine\n2.Exit");
+
+            Console.WriteLine("Enter The option");
+
+            switch (Convert.ToInt32(Console.ReadLine()))
+            {
+                case 1:
+                    VendingMachine.ReadInput();
+                    break;
+                case 2:
+                    Console.WriteLine("Exited");
+                    break;
+                default:
+                    Console.WriteLine("Enter the correct option");
+                    break;
+            }
+        }
+    }
+}
---
+++ VendingMachine/VendingMachine.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VendingMachine
+{
+    class VendingMachine
+    {
+        public static void ReadInput()
+        {
+            int amount;
+            Console.WriteLine("Enter the amount");
+            amount = Convert.ToInt32(Console.ReadLine());
+            GetMinimumNotes(amount);
+
+
+        }
+
+        //method for get minimum notes
+        private static void GetMinimumNotes(int amount)
+        {
+            //Notes array carries demoninations
+            int[] notes = { 1000, 500, 100, 50, 10, 5, 2, 1 };
+
+            //this array counts number of notes
+            int[] count = new int[notes.Length];
+
+            //to calculate notes required
+            for (int i = 0; i < notes.Length - 1; i++)
+            {
+                if (amount >= notes[i])
+                {
+                    count[i] = amount / notes[i];
+                    amount -= (count[i] * notes[i]);
+                }
+            }
+            Console.WriteLine($"Total Notes Required are : {count.Length}");
+            Console.WriteLine("The notes are:");
+            //To print notes
+            for (int i = 0; i < notes.Length - 1; i++)
+            {
+                if (count[i] != 0)
+                {
+                    Console.WriteLine($"{count[i]} --> Rs.{notes[i]} Required");
+                }
+            }
+
+        }
+    }
+}
